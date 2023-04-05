b80dfc5b3f0478539e6983dbdf44531c28818986
ErezAmihud <63999403+ErezAmihud@users.noreply.github.com>
2023-04-05 16:51:12 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -10,7 +10,7 @@ sudo apt-get build-dep -y python3
 sudo apt-get install -y  --no-install-recommends pkg-config build-essential gdb lcov pkg-config \
       libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
       libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
-      lzma lzma-dev tk-dev uuid-dev zlib1g-dev
+      lzma lzma-dev tk-dev uuid-dev zlib1g-dev libxml2 libxslt1.1 libxml2-dev libxslt1-dev python-libxml2 python-libxslt1 python-dev python-setuptools
 
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.2
@@ -26,8 +26,8 @@ asdf install nodejs latest
 asdf global nodejs latest
 
 asdf plugin-add python
-asdf install python latest:3
-asdf global python latest:3
+asdf install python latest:3.10
+asdf global python latest:3.10
 
 asdf plugin add black https://github.com/amrox/asdf-pyapp.git
 asdf plugin add conan https://github.com/amrox/asdf-pyapp.git	
