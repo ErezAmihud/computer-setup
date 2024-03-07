f2f6dce5b529bc7894176b612c7bea4f8c6eef3d
ErezAmihud <63999403+ErezAmihud@users.noreply.github.com>
2024-03-07 23:21:15 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -14,8 +14,8 @@ winget install "Windows Terminal" --accept-package-agreements --accept-source-ag
 winget install "Telegram Desktop" --accept-package-agreements --accept-source-agreements
 
 # Internet 
-choco install firefox -y
 choco install googlechrome -y
+choco install brave -y
 
 #dev
 choco install git -y
@@ -24,8 +24,6 @@ choco install vscode -y
 choco install python --version=3.10.8 -y
 choco install nodejs --version=18.15.0 -y
 choco install llvm -y
-
-choco install visualstudio2022buildtools -y --add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.NodeBuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.TestTools.BuildTools --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset
 
 # wsl 
 wsl --update --web-download
@@ -37,21 +35,19 @@ choco install docker-desktop -y
 #essensials
 choco install 1password -y
 choco install 7zip -y
-choco install everything -y
 choco install aria2 -y
 choco install curl -y
 choco install wget -y
 
 # extra
 choco install -y wireshark
-choco install grammarly-for-windows -y
+#choco install grammarly-for-windows -y
 choco install vlc -y
 choco install zeal -y
-choco install texstudio -y
+#choco install texstudio -y
 choco install windirstat -y
 choco install windscribe -y
-choco install obsidian -y
-choco install act-cli -y
+
 
 # communication
 choco install discord.install -y
@@ -67,5 +63,9 @@ reg add HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\Inpro
 # setup keyboards from https://github.com/ErezAmihud/dvorak-hebrew-keyboard-mappings
 # download zeal docs
 # Download zeal msdn from https://rotemy.com/dash/
+# also download:
+# choco install obsidian -y
+# choco install everything -y
+# choco install visualstudio2022buildtools -y --add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.NodeBuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.TestTools.BuildTools --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset
 
 .\set_git.ps1
